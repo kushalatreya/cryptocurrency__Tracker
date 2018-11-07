32e377c0c8031c55005734334143704315a3eade
kushal atreya <kushalatreya@live.com>
2018-11-07 16:38:34 +0200

    search feature embedded, add more filter

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ const Coins = props => {
         <div key={coin.id} className="coin">
           <div>{coin.rank}</div>
           <div>{coin.name}</div>
-          <div>{Number(coin.price_usd).toFixed(2)}</div>
-          
+          <div>${Number(coin.price_usd).toFixed(7)}</div>
           <div>{coin.symbol}</div>
         </div>
       );
@@ -26,6 +25,7 @@ const Coins = props => {
 class App extends Component {
   state = {
     coins: null,
+    searchKey: ''
   };
   componentWillMount() {
     console.log('log will mount');
@@ -40,20 +40,50 @@ class App extends Component {
     console.log('Component did mount log');
   }
 
+  searchEventHandler = (e) => {
+    this.setState({ searchKey: e.target.value })
+  }
+
+  searchResult = (searchKeyword) => { // search coins that includes the search key word		
+    return this.state.coins.filter(coin => coin.name.toLowerCase().includes(searchKeyword.toLowerCase()));
+
+  }
+
+  componentDidUpdate() {
+    console.log(this.searchResult(this.state.searchKey));
+  }
+  
   render() {
     const coins = this.state.coins;
-    console.log(coins);
+    let key = this.state.searchKey;
+    const filterCoins = () => {
+      console.log("key coming?", key);
+      let filterdcoins = this.searchResult(key).map(coin => {
+        console.log("filtered coin", coin)
+        return <div key={coin.id} className="coin">
+          <div>{coin.rank}</div>
+          <div>{coin.name}</div>
+          <div>${Number(coin.price_usd).toFixed(7)}</div>
+          <div>{coin.symbol}</div>
+        </div>
+      })
+      return filterdcoins;
+    }
+
+    const displayResult = (key) ? <div>{filterCoins()}</div> : <Coins coins={coins} />;
+    //console.log(coins);
     // console.log (coins.length);
 
     console.log('Loging from render method');
     return (
       <div className="App">
-        <h1 id = 'heading-text'>Cryptocurrency can change your life . . .</h1>
-        <div className="coins">
-          <Coins coins={coins} />;
-
+        <h1 id='heading-text'>Cryptocurrency can change your life . . .</h1>
+        <div className="search-input-div">
+          <input onChange={this.searchEventHandler} value={this.state.searchKey} className="search-input" type="text" placeholder="search . . ." />
         </div>
-
+        <div className="coins">
+        </div>
+        {displayResult}
       </div>
     );
   }
