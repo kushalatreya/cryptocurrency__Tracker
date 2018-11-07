a8be6736bf9402b605d1a95c8b71157b9af321f6
kushal atreya <kushalatreya@live.com>
2018-11-07 14:47:10 +0200

    api is working, do burndown chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//Mount, Updating , unMounting phases
+// Some component life cycles are depricated or change
+
+const Coins = props => {
+  const coins = props.coins != null
+    ? props.coins.map(coin => {
+      return (
+        <div key={coin.id} className="coin">
+          <div>{coin.rank}</div>
+          <div>{coin.name}</div>
+          <div>{Number(coin.price_usd).toFixed(2)}</div>
+          
+          <div>{coin.symbol}</div>
+        </div>
+      );
+    })
+    : <div> <h1>Loading.....</h1></div>;
+
+  return coins;
+};
+
+
 class App extends Component {
+  state = {
+    coins: null,
+  };
+  componentWillMount() {
+    console.log('log will mount');
+  }
+  componentDidMount() {
+    const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=200';
+    fetch(url).then(response => response.json()).then(coins => {
+      this.setState({
+        coins: coins,
+      });
+    });
+    console.log('Component did mount log');
+  }
+
   render() {
+    const coins = this.state.coins;
+    console.log(coins);
+    // console.log (coins.length);
+
+    console.log('Loging from render method');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 id = 'heading-text'>Cryptocurrency can change your life . . .</h1>
+        <div className="coins">
+          <Coins coins={coins} />;
+
+        </div>
+
       </div>
     );
   }
